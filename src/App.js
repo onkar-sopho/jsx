a29d685017841108b6ca4668c1f353aefecd3e41
Onkar <onkar21.rai@gmail.com>
2024-05-20 09:37:11 +0530

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <textarea autoFocus={true} spellCheck placeholder='How are you feeling todayz?' />
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// 1. Import React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MyApp from './App';
+
+// 2. Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+// 3. Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4. Show the component on the screen
+root.render(<MyApp />);   
